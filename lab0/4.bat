7eab5118e31d3d9a5f3b720dd049c0e932d56f80
mamsdeveloper <belk1naalyona@yandex.ru>
2022-09-17 03:41:10 +0300

    lab0 | some scripts and report chnges

--- lab0/4.bat
+++ lab0/4.bat
@@ -1,21 +1,20 @@
 cd lab0 
 
 :: 1
-wc -l archeops3 | head -c 8 | tail -c 1 >> archeops3
-echo \ >> archeops3
+wc -l archeops3 | head -c 8 | tail -c 1 1>> archeops3
+echo \ 1>> archeops3
 
 :: 2
 ls -Rt1l poochyena9
 
 :: 3
-grep -n -i te archeops3 2> /dev/null
+grep -ni te archeops3 2> /dev/null
 
 :: 4
-mkdir tmp
-grep --include=\*o -rh ".*" . | sort 2> tmp/errors
+grep --include=\*o -rh ".*" . | sort 2> /tmp/s3467945_errors
 
 :: 5
-grep --include=\p* -rl  ".*" . |xargs  ls -lt 2> tmp/errors
+grep --include=\p* -rl  ".*" . |xargs ls -lc 2> /tmp/s3467945_errors
 
 :: 6
 grep -rhi ".*[^h]$" ./poochyena9/ 2> /dev/null
